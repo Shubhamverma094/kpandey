c36b44d371d9aa48073c2f637fddf3e5928022fb
Shubhamverma <shubhamverma08726@gmail.com>
2024-10-03 23:55:13 +0530

    first commit

---
+++ js/script.js
@@ -0,0 +1,8 @@
+let icons = document.querySelector(".icon");
+let ul = document.querySelector("ul");
+
+icons.addEventListener("click",function(){
+    ul.classList.toggle("show")
+    console.log("Hello icons");
+    
+})
